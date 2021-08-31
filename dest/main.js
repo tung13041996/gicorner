9e665f634e7f46094930b82983c8a9c1aa057f60
Nguyen Thanh Tung <nguyenthanhtungbk96@gmail.com>
2021-08-31 20:46:20 +0700

    import GSAP library

--- dest/main.js
+++ dest/main.js
@@ -1,1 +1,19 @@
-console.log('Hello CFD');
+$(document).ready(function() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    let timeline = gsap.timeline({
+        scrollTrigger: {
+            trigger:"body",
+            start:"top top",
+            end:"bottom bottom",
+            scrub: true,
+        }
+    });
+
+    timeline.to($(".square"), {
+        rotate: "360deg",
+        backgroundColor:"#ff0000",
+        borderRadius: "50%",
+        duration: 1,
+    },0);
+})
