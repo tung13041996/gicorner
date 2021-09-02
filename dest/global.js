9d61a919627966de2238e1bdfff72ec42dc82d71
Nguyen Thanh Tung <nguyenthanhtungbk96@gmail.com>
2021-09-03 01:22:04 +0700

    animation loading page

--- dest/global.js
+++ dest/global.js
@@ -48,7 +48,97 @@ $(document).ready(function () {
         });
     }
 
+    /*loading page*/
+    function loadingAnimation(options) {
+        let settings = $.extend({
+                wrapper: '', // element wrap lines
+                total: 10, // number of lines
+                oddBg: 'red', // color code of odd line
+                evenBg: 'blue', // color code of even line
+                lineStagger: .03, // delay between each line
+                lineDuration: .5
+            }, options),
+            timeline = gsap.timeline(),
+            $oddLines, $evenLines;
+
+        // if wrapper doesn't exist
+        if (!settings.wrapper.length) return;
+
+        // create lines
+        for (let i = 0; i < settings.total; i++) {
+            let style = `height:${settings.wrapper.height() / settings.total}px;`;
+            style += `background-color:${i % 2 === 0 ? settings.oddBg : settings.evenBg}`;
+            $(`<div class="line" style="${style}"></div>`).appendTo(settings.wrapper);
+        }
+
+        $oddLines = settings.wrapper.find('.line:nth-child(odd)');
+        $evenLines = settings.wrapper.find('.line:nth-child(even)');
+
+
+        //move odd line to main screen
+        // staggerFromTo (oddLine reverse, 0s, {x=-100%}, {x=0})
+        timeline.staggerFromTo($oddLines.get().reverse(), settings.lineDuration,
+            {x: '100%'},
+            {
+                x: 0,
+                stagger: function (index) {
+                    return (2 * index + 1) * settings.lineStagger;
+                }
+            },
+            {
+                each:0,
+                amount:0
+            },
+            0
+        );
+
+
+        //move even line to main screen
+        // staggerFromTo (evenLine reverse, 0.2s, {x=-100%}, {x=0})
+        timeline.staggerFromTo($evenLines.get().reverse(), settings.lineDuration,
+            {x: '100%'},
+            {
+                x: 0,
+                stagger: function (index) {
+                    return (2 * index + 1) * settings.lineStagger;
+                }
+            },
+            {
+                each:0,
+                amount:0
+            },
+            settings.lineDuration,
+        );
+
+        // set background for wrapper like background color of even line
+        timeline.to(settings.wrapper, {backgroundColor: settings.evenBg, duration: 0});
+
+
+        //move even line to main screen
+        // staggerFromTo (evenLine reverse, after even before run, {x=-100%}, {x=100%})
+
+        timeline.staggerTo($oddLines.get().reverse(), settings.lineDuration,
+            {
+                x: '-100%',
+                stagger: function (index) {
+                    return (2 * index + 1) * settings.lineStagger;
+                }
+            },
+        );
+
+        // hide wrapper
+        timeline.to(settings.wrapper, {autoAlpha: 0, duration: .3});
+    }
+
 
     // init function
     mouseMove();
+    loadingAnimation({
+        wrapper: $('.line-animation-wrapper'),
+        total: 20,
+        oddBg: '#108241',
+        evenBg: '#d7f1ef',
+        lineStagger: .02,
+        lineDuration: .4
+    })
 });
